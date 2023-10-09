71af5bc205110e50969a7527f2265ddd3831f87d
Your Name <you@example.com>
2023-10-09 12:03:49 -0700

    login cubit

--- lib/cubit/login_cubit/login_cubit.dart
+++ lib/cubit/login_cubit/login_cubit.dart
@@ -1,8 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:meta/meta.dart';
 
 part 'login_state.dart';
 
 class LoginCubit extends Cubit<LoginState> {
   LoginCubit() : super(LoginInitial());
+
+  Future<void> loginUser(
+      {required String email, required String password}) async {
+    emit(LoginLoading());
+    try {
+      var auth = FirebaseAuth.instance;
+      UserCredential user = await auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      emit(LoginSuccess());
+    } on Exception catch (e) {
+      emit(LoginFailure());
+    }
+  }
 }
